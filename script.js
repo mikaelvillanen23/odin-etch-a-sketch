43be748adfc5e3cb342c67b7f2cebbb637f1a81e
mikaelvillanen23 <mikael.villanen23@gmail.com>
2026-03-28 22:24:05 +0200

    Add createCanvas() and toggleGrid() functions
    This commit adds functions for generating a fresh
    canvas from user input and toggling canvas grid
    visibility, and the corresponding html buttons and
    css.
    
    createCanvas() still lacks conditions
    for making sure the input is valid and perhaps
    defaulting to some value if not.

--- script.js
+++ script.js
@@ -8,25 +8,61 @@ window.addEventListener("pointerup", () => {
   pointerHeld = false;
 });
 
-const squares = document.querySelectorAll(".square");
+function createCanvas() {
+  let n = Number(prompt("Determine canvas size (min = 10, max = 100)"));
 
-squares.forEach((square) => {
-  square.addEventListener("pointerdown", () => {
-    // to color the clicked square itself
-    square.classList.add("colored");
+  const container = document.querySelector("#container");
+  container.innerHTML = ""; // remove previous squares
+
+  for (let i = 0; i < n; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+
+    for (let j = 0; j < n; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.addEventListener("pointerdown", () => {
+        square.classList.add("colored"); // to color the clicked square itself
+      });
+      square.addEventListener("pointerover", () => {
+        if (pointerHeld === true) square.classList.add("colored");
+      });
+      row.appendChild(square);
+    }
+  }
+}
+
+const canvasButton = document.querySelector("#canvasButton");
+canvasButton.addEventListener("click", createCanvas);
+
+const gridButton = document.querySelector("#gridButton");
+gridButton.addEventListener("click", toggleGrid);
+
+function toggleGrid() {
+  const squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    square.classList.toggle("grid");
   });
-  square.addEventListener("pointerover", () => {
-    if (pointerHeld === true) square.classList.add("colored");
-  });
-});
+  if (gridButton.textContent === "Show grid") {
+    gridButton.textContent = "Hide grid";
+  } else gridButton.textContent = "Show grid";
+}
+
+
 
 
 /* TODO:
-- copy relevant code to repo files + commit
-- creation of grid with flexbox out of user input (accept numbers only + min = 16, max = 100)
++ creation of grid with flexbox out of user input
+  -accept numbers only -> if something else alert "numbers only pls"
+  -min = 10, max = 100 -> if OB, default to min/max
+- create default canvas of 16x16
 - additional functionalities:
+  - clear current
   - eraser with secondary click!
   - colorpicker?
   - randomizer for color?
   - opacity?
+  + show/hide grid
 */
