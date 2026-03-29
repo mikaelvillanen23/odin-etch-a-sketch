d71fa419be2c0f3f69aab0bf319929acb71d85c3
mikaelvillanen23 <mikael.villanen23@gmail.com>
2026-03-29 23:54:57 +0300

    Add resetCanvas() function
    This commit adds resetCanvas() to initialize
    the canvas at page load and to reset it when the
    corresponding button is clicked.
    
    Also added for createCanvas() a check if user
    input matches given range of possible values,
    and of not, the default outcome.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // let currentColor = "black"
 let pointerHeld = false;
+let currentSize = 16;
 
 window.addEventListener("pointerdown", () => {
   pointerHeld = true;
@@ -8,10 +9,13 @@ window.addEventListener("pointerup", () => {
   pointerHeld = false;
 });
 
-function createCanvas() {
-  let n = Number(prompt("Determine canvas size (min = 10, max = 100)"));
+const container = document.querySelector("#container");
+const resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener("click", () => {
+  resetCanvas(currentSize)
+});
 
-  const container = document.querySelector("#container");
+function resetCanvas(n) {
   container.innerHTML = ""; // remove previous squares
 
   for (let i = 0; i < n; i++) {
@@ -22,8 +26,8 @@ function createCanvas() {
     for (let j = 0; j < n; j++) {
       const square = document.createElement("div");
       square.classList.add("square");
-      square.addEventListener("pointerdown", () => {
-        square.classList.add("colored"); // to color the clicked square itself
+      square.addEventListener("pointerdown", () => { // to color the clicked square itself
+        square.classList.add("colored"); 
       });
       square.addEventListener("pointerover", () => {
         if (pointerHeld === true) square.classList.add("colored");
@@ -31,6 +35,37 @@ function createCanvas() {
       row.appendChild(square);
     }
   }
+  if (gridButton.textContent === "Hide grid") gridButton.textContent = "Show grid";
+}
+
+function createCanvas() {
+  container.innerHTML = ""; // remove previous squares
+
+  let n = Number(prompt("Determine canvas side length (min = 10, max = 100).\nIf you fail to hit the given range, size will default to 16x16."));
+  currentSize = n;
+
+  if (n < 10 || n > 100) {
+    resetCanvas(16);
+  } else {
+      for (let i = 0; i < n; i++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+      container.appendChild(row);
+
+      for (let j = 0; j < n; j++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.addEventListener("pointerdown", () => { // to color the clicked square itself
+          square.classList.add("colored"); 
+        });
+        square.addEventListener("pointerover", () => {
+          if (pointerHeld === true) square.classList.add("colored");
+        });
+        row.appendChild(square);
+      }
+    }
+  }
+  if (gridButton.textContent === "Hide grid") gridButton.textContent = "Show grid";
 }
 
 const canvasButton = document.querySelector("#canvasButton");
@@ -50,16 +85,16 @@ function toggleGrid() {
   } else gridButton.textContent = "Show grid";
 }
 
-
+resetCanvas(16);
 
 
 /* TODO:
 + creation of grid with flexbox out of user input
-  -accept numbers only -> if something else alert "numbers only pls"
-  -min = 10, max = 100 -> if OB, default to min/max
-- create default canvas of 16x16
-- additional functionalities:
-  - clear current
+  +accept numbers only -> if something else alert "numbers only pls"
+  +min = 10, max = 100 -> if OB, default to min/max
++ create default canvas of 16x16
+Additional functionalities:
+  + clear current
   - eraser with secondary click!
   - colorpicker?
   - randomizer for color?
