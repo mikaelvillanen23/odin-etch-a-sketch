64a767d7324a016b7ab39173c2770bba1390de30
mikaelvillanen23 <mikael.villanen23@gmail.com>
2026-03-31 00:29:55 +0300

    Add eraser functionality for mouse right-click
    Also refactored createCanvas() so that it can be
    used for resetting the canvas as well by making the
    prompt happen through a helper function userInput().
    This way resetCanvas() could be removed entirely
    which only makes sense since it was almost identical.

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 // *** Variables and DOM elements ***
 
 // let currentColor = "black"
-let pointerHeld = false;
+let pointer0Held = false;
+let pointer2Held = false;
 let currentSize = 16;
 
-window.addEventListener("pointerdown", () => {
-  pointerHeld = true;
+window.addEventListener("mousedown", (event) => {
+  switch (event.button) {
+    case 0: // mouse left
+      pointer0Held = true;
+      break;
+    case 2: // mouse right
+      pointer2Held = true;
+      break;
+  }
 });
-window.addEventListener("pointerup", () => {
-  pointerHeld = false;
+window.addEventListener("mouseup", () => {
+  pointer0Held = false;
+  pointer2Held = false;
 });
 
 const container = document.querySelector("#container");
 const resetButton = document.querySelector("#resetButton");
 resetButton.addEventListener("click", () => {
-  resetCanvas(currentSize)
+  createCanvas(currentSize)
 });
 const canvasButton = document.querySelector("#canvasButton");
-canvasButton.addEventListener("click", createCanvas);
+canvasButton.addEventListener("click", () => {
+  createCanvas(userInput());
+});
 const gridButton = document.querySelector("#gridButton");
 gridButton.addEventListener("click", toggleGrid);
 const sizeInfo = document.querySelector(".currentSize");
@@ -26,36 +37,16 @@ sizeInfo.textContent = `${currentSize} x ${currentSize}`;
 
 // *** Function definitions ***
 
-function resetCanvas(n) {
-  container.innerHTML = ""; // remove previous squares
-
-  for (let i = 0; i < n; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-
-    for (let j = 0; j < n; j++) {
-      const square = document.createElement("div");
-      square.classList.add("square");
-      square.addEventListener("pointerdown", () => { // to color the clicked square itself
-        square.classList.add("colored"); 
-      });
-      square.addEventListener("pointerover", () => {
-        if (pointerHeld === true) square.classList.add("colored");
-      });
-      row.appendChild(square);
-    }
-  }
-  if (gridButton.textContent === "Hide grid") gridButton.textContent = "Show grid";
+function userInput() {
+  let n = Number(prompt("Determine canvas side length (min = 10, max = 100).\n\nNote:\nIf you fail to hit the given range or press Cancel, canvas size will default to 16x16."));
+  return n;
 }
 
-function createCanvas() {
+function createCanvas(n) {
   container.innerHTML = ""; // remove previous squares
 
-  let n = Number(prompt("Determine canvas side length (min = 10, max = 100).\n\nNote:\nIf you fail to hit the given range or press Cancel, canvas size will default to 16x16."));
-
   if (n < 10 || n > 100 || isNaN(n)) {
-    resetCanvas(16);
+    createCanvas(16);
     currentSize = 16;
   } else {
       currentSize = n;
@@ -67,11 +58,25 @@ function createCanvas() {
       for (let j = 0; j < n; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
-        square.addEventListener("pointerdown", () => { // to color the clicked square itself
-          square.classList.add("colored"); 
+        square.addEventListener("mousedown", (event) => { // to (de)color the clicked square itself
+          switch (event.button) {
+            case 0:
+              square.classList.add("colored");
+              break;
+            case 2:
+              square.classList.remove("colored");
+              break;
+          }
         });
-        square.addEventListener("pointerover", () => {
-          if (pointerHeld === true) square.classList.add("colored");
+        square.addEventListener("mouseover", () => {
+          if (pointer0Held === true) {
+            square.classList.add("colored");
+          } else if (pointer2Held === true) {
+            square.classList.remove("colored");
+          }
+        });
+        square.addEventListener("contextmenu", function (event) {
+          event.preventDefault();
         });
         row.appendChild(square);
       }
@@ -92,12 +97,12 @@ function toggleGrid() {
   } else gridButton.textContent = "Show grid";
 }
 
-resetCanvas(16);
+createCanvas(16);
 
 
 /* TODO:
+  - info for UI
 Additional functionalities:
-  - eraser with secondary click?
   - colorpicker?
   - randomizer for color?
   - opacity?
